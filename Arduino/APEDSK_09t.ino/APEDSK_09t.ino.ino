6c62ce098a9b7b0a58c4f77beac7edc2135c04d4
Jochen Buur <53290559+jambuur@users.noreply.github.com>
2019-08-29 14:21:58 +1200

    Update APEDSK_09t.ino.ino

--- Arduino/APEDSK_09t.ino/APEDSK_09t.ino.ino
+++ Arduino/APEDSK_09t.ino/APEDSK_09t.ino.ino
@@ -9,6 +9,18 @@
 * The Arduino RAM interface is based on Mario's EEPROM programmer:
 * https://github.com/mkeller0815/MEEPROMMER
 *
+* faster digitalRead/Write by:
+* http://masteringarduino.blogspot.com/2013/10/fastest-and-smallest-digitalread-and.html
+* usage:
+*   pinMode( pin, INPUT ); with pinAsInput( pin );
+*   pinMode( pin, OUTPUT ); with pinAsOutput( pin );
+*   pinMode( pin, INPUT_PULLUP); with pinAsInputPullUp( pin );
+*   digitalWrite( pin, LOW ); with digitalLow( pin );
+*   digitalWrite( pin, HIGH ); with digitalHigh( pin );
+*   digitalRead( pin ) with digitalState( pin )
+*   if( digitalState( pin ) == HIGH )  can be if( isHigh( pin ) ) 
+*   digitalState( pin ) == LOW         can be isLow( pin )
+*
 * $Author: Jochen Buur $
 * $Date: Aug 2019 $
 * $Revision: 0.12k $
@@ -18,6 +30,25 @@
 * Use this code at your own risk. 
 * I'm not responsible for any bad effect or damages caused by this software
 */
+
+//faster digitalRead / digitalWrite definitions
+#define portOfPin(P)\
+  (((P)>=0&&(P)<8)?&PORTD:(((P)>7&&(P)<14)?&PORTB:&PORTC))
+#define ddrOfPin(P)\
+  (((P)>=0&&(P)<8)?&DDRD:(((P)>7&&(P)<14)?&DDRB:&DDRC))
+#define pinOfPin(P)\
+  (((P)>=0&&(P)<8)?&PIND:(((P)>7&&(P)<14)?&PINB:&PINC))
+#define pinIndex(P)((uint8_t)(P>13?P-14:P&7))
+#define pinMask(P)((uint8_t)(1<<pinIndex(P)))
+
+#define pinAsInput(P) *(ddrOfPin(P))&=~pinMask(P)
+#define pinAsInputPullUp(P) *(ddrOfPin(P))&=~pinMask(P);digitalHigh(P)
+#define pinAsOutput(P) *(ddrOfPin(P))|=pinMask(P)
+#define digitalLow(P) *(portOfPin(P))&=~pinMask(P)
+#define digitalHigh(P) *(portOfPin(P))|=pinMask(P)
+#define isHigh(P)((*(pinOfPin(P))& pinMask(P))>0)
+#define isLow(P)((*(pinOfPin(P))& pinMask(P))==0)
+#define digitalState(P)((uint8_t)isHigh(P))
 
 //SPI and SD card functions
 #include <SPI.h>
