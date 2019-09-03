61fa53b15710fecf537d1d85b18d8039f90511e3
jambuur <53290559+jambuur@users.noreply.github.com>
2019-09-03 21:03:50 +1200

    Update APEDSK_09t.ino.ino

--- Arduino/APEDSK_09t.ino/APEDSK_09t.ino.ino
+++ Arduino/APEDSK_09t.ino/APEDSK_09t.ino.ino
@@ -88,12 +88,13 @@ volatile byte FD1771 = 0;
 //generic variable for R/W RAM
 byte DSRAM  = 0;
 
-byte ccmd   = 0; //current command
-byte lcmd   = 0; //last command
-byte ccruw  = 0; //current CRUWRI value
-byte lcruw  = 0; //last CRUWRI value
-int lrdi    = 0; //byte counter for sector/track R/W
-int secval  = 0; //absolute sector number: (side * 359) + (track * 9) + WSECTR
+byte ccmd         = 0; //current command
+byte lcmd         = 0; //last command
+byte ccruw        = 0; //current CRUWRI value
+byte lcruw        = 0; //last CRUWRI value
+int lrdi          = 0; //byte counter for sector/track R/W
+int secval        = 0; //absolute sector number: (side * 359) + (track * 9) + WSECTR
+long int btidx    = 0; //absolute byte index: (secval * 256) + repeat R/W
   
 boolean curdir = LOW; //current step direction, step in(wards) by default
 
@@ -117,12 +118,9 @@ boolean curdir = LOW; //current step direction, step in(wards) by default
 
 //input and output file pointers
 File InDSR;   //DSR binary
-File InDSK1;  //DSK1 read
-File OutDSK1; //DSK1 write
-File InDSK2;  //DSK2 read
-File OutDSK2; //DSK2 write
-File InDSK3;  //DSK3 read
-File OutDSK3; //DSK3 write
+
+//DSKx file pointers; x=1,2,3 for read, (x+3) for write
+File DSK[7]; 
 
 //flags for additional "drives" (aka DOAD files) available
 boolean DSK2 = LOW;
@@ -386,21 +384,28 @@ void setup() {
   }
 
   //open DSK1 (error if doesn't exist)
-  InDSK1 = SD.open("/DISKS/001.DSK", FILE_READ);
-    if (!InDSK1) {
-      eflash(3);
-    }
+  DSK[1] = SD.open("/DISKS/001.DSK", FILE_READ);
+  if (!DSK[1]) {
+    eflash(3);
+  }
+  else {
+    DSK[1+3] = SD.open("/DISKS/001.DSK", FILE_WRITE);
+  }
+  
   //try to open DSK2 and flag if exists
-  InDSK2 = SD.open("/DISKS/002.DSK", FILE_READ);
-    if (InDSK2) {
-      DSK2 = HIGH;
-    }
-  //try top open DSK3 and flag if exists
-  InDSK3 = SD.open("/DISKS/003.DSK", FILE_READ);  
-    if (InDSK3) {
-      DSK3 = HIGH;
-    }
-
+  DSK[2] = SD.open("/DISKS/002.DSK", FILE_READ);
+  if (DSK[2]) {
+    DSK[2+3] = SD.open("/DISKS/002.DSK", FILE_WRITE);
+    DSK2 = HIGH;
+  }
+   
+  //try to open DSK3 and flag if exists
+  DSK[3] = SD.open("/DISKS/003.DSK", FILE_READ);
+  if (DSK[3]) {
+    DSK[3+3] = SD.open("/DISKS/003.DSK", FILE_WRITE);
+    DSK3 = HIGH;
+  }
+  
   //initialise FD1771 (clear CRU and FD1771 registers)
   for (unsigned int ii = CRURD; ii < (WDATA+1) ; ii++) {
     Wbyte(ii,0x00);
@@ -424,20 +429,20 @@ void loop() {
     //first test if write was updating the CRU Write Register as we can go straight back to the TI
     if ( (Rbyte(CRUWRI) == lcruw) {
 
-      //nope so prep with reading Command Register and strip off unneeded floppy bits
+      //nope so prep with reading Command Register
       DSRAM = Rbyte(WCOMND);
         
-      if ( DSRAM != lcmd ) {    
-          ccmd = DSRAM; //new command
+      if ( DSRAM != lcmd ) {    //same as last command?
+          ccmd = DSRAM;         //nope, new command
       }
-      else if ( Rbyte(RDINT+1) != lrdi ) {
-        ccmd = lcmd;  //updated read counter
+      else if ( Rbyte(RDINT+1) != lrdi ) {  //updated read counter?
+        ccmd = lcmd;                        //yes, continue with same command
       }
       else {
-        ccmd = 32;  //update to Data, Sector or Track Register; no further action this time (same as Step)
+        ccmd = 32;  //was update to Data, Sector or Track Register; no further action this time (same as Step)
       }
             
-      switch (ccmd) {  //strip unneeded floppy bits
+      switch (ccmd & 0xF0) {  //keep command only, strip unneeded floppy bits
 	
        	case 0: //restore
           Wbyte(RTRACK,0);  //clear read track register
@@ -460,7 +465,7 @@ void loop() {
   	      //don't have to do anything for just step
   		  break;	    
   	
-      	case 48: //step+T
+      	case 48: //step+T (update Track register)
       		DSRAM = Rbyte(RTRACK);	//read current track #
       		//is current direction inwards and track # still within limits?
       		if ( curdir == LOW && DSRAM < 39 ) {
@@ -472,11 +477,11 @@ void loop() {
       		}			
         break;
        
-    	  case 64: //step-in
+    	  case 64: //step-in (towards track 39)
     		  curdir == LOW; //set current direction		
         break;
   	
-      	case 80: //step-in+T
+      	case 80: //step-in+T (towards track 39, update Track Register)
       		DSRAM = Rbyte(RTRACK);  //read current track #
       		//if track # still within limits update track register
       		if ( DSRAM < 39) { 
@@ -485,7 +490,7 @@ void loop() {
       		curdir == LOW; //set current direction		
         break;
   
-        case 96: //step-out
+        case 96: //step-out (towards track 0)
     		  curdir == HIGH; //set current direction		
         break;
     
@@ -497,16 +502,15 @@ void loop() {
           }
           curdir == HIGH; //set current direction    
         break;
-/*      
+     
   	    case 128: //read sector
-           if ( ccmd != lcmd ) { //new sector read
-            //determine absolute sector #
+          if ( ccmd != lcmd ) { //new sector read     
+            //absolute sector calc: (side * 39) + (track * 9) + sector #
+            secval = ( (Rbyte(CRURD) >> 7) * 39) + (Rbyte(RTRACK) * 9) + Rbyte(RSECTR);
             
-            
-            secval = ( (Rbyte(CRURD
             
           }
-  			secval = (side * 359) + (track * 9) + WSECTR
+  			
 			
  while m is set AND WSECTR < max
           while RDINT >= 0
@@ -544,7 +548,7 @@ void loop() {
 	      break;
 	    case 240: //write track
         while sector <  sectors p/t
-	      break; */
+	      break; 
       }
     }
   }
@@ -559,7 +563,7 @@ void loop() {
   
   FD1771 = 0;   //clear interrupt flag
   interrupts(); //enable interrupts again
-
+*/
 } //end of loop()
 
 void listen1771() {
