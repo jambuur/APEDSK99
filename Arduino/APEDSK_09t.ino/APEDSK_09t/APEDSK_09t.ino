087bc87ea36420a78f2f3d2f652bf4d63108571a
jambuur <53290559+jambuur@users.noreply.github.com>
2019-11-29 20:45:39 +1300

    Update APEDSK_09t.ino

--- Arduino/APEDSK_09t.ino/APEDSK_09t/APEDSK_09t.ino
+++ Arduino/APEDSK_09t.ino/APEDSK_09t/APEDSK_09t.ino
@@ -379,7 +379,13 @@ void RWsector( boolean rw ) {
         DSRAM++;                                      //no; increase Sector #
         Wbyte(WSECTR, DSRAM);                         //sync Sector Registers
         Wbyte(RSECTR, DSRAM);                         //""
-        Sbtidx = 1;                                   //reset sector byte counter
+        if ( rw ) {                                   //read 1st byte of new sector?
+          Wbyte(RDATA, DSK[cDSK].read() );            //yes -> supply next byte
+        }
+        else {
+          DSK[cDSK].write( Rbyte(WDATA) );            //no -> write next byte to DOAD
+        }    
+        Sbtidx = 1;                                   //adjust sector byte counter
       }
       else {
         noExec();                                     //all sectors done; exit
