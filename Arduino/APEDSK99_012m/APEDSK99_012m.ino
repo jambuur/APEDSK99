643df923a7dd0197098f3604f51064d479cd9950
jambuur <53290559+jambuur@users.noreply.github.com>
2020-03-27 10:55:30 +1300

    Update APEDSK99_012m.ino

--- Arduino/APEDSK99_012m/APEDSK99_012m.ino
+++ Arduino/APEDSK99_012m/APEDSK99_012m.ino
@@ -670,25 +670,6 @@ void loop() {
 		      } 
  	        break;       
 
-          case 11:                                                            //FDSK() Show Files on DSKx
-          {
-            /*cDSK = Rbyte(DTCDSK);                                              //is the requested disk mapped to a DOAD?
-            if ( aDSK[cDSK] ) {
-              DSK[cDSK] = SD.open(nDSK[cDSK], FILE_READ);    
-            
-          
-          
-          
-          
-          
-          
-          */
-          }
-
-
-
-
-   
         } //end switch accmd commands   
       } //end check APEDSK99-specific commands                                 
     } //end else 
